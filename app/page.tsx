6a5f7406b09b856dc717b3dc5ca49aede027419c
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2023-09-18 14:27:04 -0600

    add style accord to figma

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export default function Home() {
     return (
-        <main className="h-screen flex items-center justify-center flex-col">
-            <div>Hola amigues :) un boton de shadcn pa que lo vean</div>
+        <main className="h-screen flex items-center justify-center flex-col bg-[#0F1E2F]">
+            <div className='text-[#F5F5DC]'>Hola amigues :) un boton de shadcn pa que lo vean</div>
             <Link
                 href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
                 className={buttonVariants({
