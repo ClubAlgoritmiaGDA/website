6b8253c645e22eac8a3ebc40f8b8dc59c2076896
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2023-10-04 12:11:42 -0600

    change background colorin page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Home() {
     return (
-        <main className="h-screen flex items-center justify-center flex-col">
+        <main className="h-screen flex items-center justify-center flex-col bg-algoritmia-blue">
             <div>Hola amigues :) un boton de shadcn pa que lo vean</div>
             <Link
                 href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
